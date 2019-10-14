0eee31f5d0f7c4cb4b88c226cfa8fdb0e0070ed9
Hanna <hannagoonerette@gmail.com>
2019-10-14 13:52:48 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,53 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    arr = expr.split('');
+    let arrLetter = [];
+    let j = 0;
+    for (let i = 0; i < arr.length; i = i+10){
+        arrLetter[j] = arr.slice(i , i+10); 
+        j++;
+    }
+    let charCode;
+    let result = [];
+    for (j = 0; j < arrLetter.length; j++){
+        charCode = arrLetter[j].join('');
+        let tmp = 0;
+        let comp;
+        let charMorse = [];
+        do {
+            if (charCode[0] == '*'){
+                comp=' ';
+                tmp = charCode.length+1;
+            }
+            else    
+            {
+                let currentSymbol = charCode.slice(tmp, tmp+2); 
+                switch (currentSymbol){
+                    case '10': 
+                        comp = '.';
+                        break;
+                    case '11': 
+                        comp = '-';
+                        break;
+                    case '00': 
+                        comp = '';
+                        break;
+                }
+                tmp+=2;
+            }
+        charMorse.push(comp);
+        }
+        while (tmp < charCode.length);
+        charMorse = charMorse.join('');
+        if (MORSE_TABLE[charMorse] == undefined){
+            result.push(' ');
+        }
+        else{
+            result.push(MORSE_TABLE[charMorse]);    
+        }
+    }
+    return(result.join(""));
 }
 
 module.exports = {
